50561d328ececc4ce222ac3179adacdc9e8b0cce
Lola Dolinsky <dolinsky.lola@gmail.com>
2021-01-14 16:00:53 -0700

    Install React-Router

--- src/App/index.js
+++ src/App/index.js
@@ -1,4 +1,9 @@
+import React from 'react';
+import Home from '../Home';
+import NavBar from '../NavBar';
+import GetMaterial from '../GetMaterial';
 import './App.css';
+// import { Route, Switch, Redirect } from 'react-router-dom';
 
 function App() {
   return (
