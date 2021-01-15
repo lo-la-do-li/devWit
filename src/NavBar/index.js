31e10556cdf24c2748d5c8793d2107baa0989fc5
Lola Dolinsky <dolinsky.lola@gmail.com>
2021-01-15 11:23:54 -0700

    Clean up

--- src/NavBar/index.js
+++ src/NavBar/index.js
@@ -3,19 +3,19 @@ import { Link } from 'react-router-dom';
 import './NavBar.css';
 
 function NavBar() {
-   const [value, setValue] = useState(0);
+  //  const [value, setValue] = useState(0);
 
-  const handleChange = (event) => {
-    const tabValue = event.target
-    console.log(tabValue)
-    setValue(tabValue);
-  };
+  // const handleChange = (event) => {
+  //   const tabValue = event.target
+  //   console.log(tabValue)
+  //   setValue(tabValue);
+  // };
 
   return (
     <nav 
       className="nav-bar" 
-      value={value} 
-      onChange={handleChange} 
+      // value={value} 
+      // onChange={handleChange} 
       >
       <Link to='/home'>
 				<button >Home</button>
