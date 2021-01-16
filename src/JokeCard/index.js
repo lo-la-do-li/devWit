74cd712e094bd7522e4f1f2e7a7302e936cb23b8
Lola Dolinsky <dolinsky.lola@gmail.com>
2021-01-16 13:44:51 -0700

    Display mockJokes in JokesContainer on MySet page

--- src/JokeCard/index.js
+++ src/JokeCard/index.js
@@ -1,16 +1,27 @@
-import React from 'react';
-import './JokeCard.css'
+import React from "react";
+import "./JokeCard.css";
 
 const JokeCard = ({ id, type, setup, punchline, removeJoke }) => {
-	
-	return (
-		<div className='joke-card'>
-			<h3>Joke</h3>
-				<p><b>Setup:</b> {setup}</p>
-				<p><b>Punchline:</b> {punchline}</p>
-			<button className='scrap-it-btn' onClick={() => removeJoke(id)}>SCRAP IT 🗑️</button>
-		</div>
-	)
-}
+  return (
+    <div className="joke-card">
+      <p>
+        <b>Setup:</b> {setup}
+      </p>
+      <p>
+        <b>Punchline:</b> {punchline}
+      </p>
+      <div className="button-bar">
+        <button className="scrap-it-btn" onClick={removeJoke(id)}>
+          {" "}
+          🗑️
+        </button>
+        <button className="scrap-it-btn" onClick={removeJoke(id)}>
+          {" "}
+          ➕
+        </button>
+      </div>
+    </div>
+  );
+};
 
 export default JokeCard;
